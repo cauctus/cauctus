24aa8f229232eec031bd9f7aebdfa1ef4caa9993
Corentin THOMASSET <corentin.thomasset74@gmail.com>
2021-07-27 14:11:14 +0000

    refactor(dictionary): switched nouns and adjectives to yaml
    Signed-off-by: GitHub <noreply@github.com>

--- service/theme-generator.ts
+++ service/theme-generator.ts
@@ -1,5 +1,7 @@
-import {nouns, adjectives} from '~/data/dictionary'
 import {capitalize, randFromArray} from '~/helpers/utils'
+
+import nouns from '~/data/nouns.yaml'
+import adjectives from '~/data/adjectives.yaml'
 
 const generateTheme = () => capitalize(`${randFromArray(nouns)} ${randFromArray(adjectives)}`)
 
